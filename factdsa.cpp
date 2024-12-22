6f4f4b84d79a783c4e0526a296b17687d0f69974
PranavMishra07 <pranav072003@gmail.com>
2024-12-22 15:13:21 +0530

    l

---
+++ factdsa.cpp
@@ -0,0 +1,78 @@
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int n,fact=1;
+//	cout<<"\nEnter any Number : ";
+//	cin>>n;
+//	for(int i=1;i<=n;i++){
+//		fact=fact*i;
+//	
+//	}
+//		cout<<fact;
+//	
+//	
+//}
+//#include<iostream>
+//using namespace std;
+//    fact(int n){
+//if(n==1){
+//	return 1;
+//	}    	
+//else{
+//	return n*fact(n-1);
+//}
+//
+//return 0;
+//	}
+//int main(){
+//	int n;
+//	cout<<"\nEnter Any Number : ";
+//	cin>>n;
+//cout<<"\nFactorial of Number Is :"<<fact(n);
+//	
+//}
+#include <iostream>
+
+using namespace std;
+
+class Parent{
+public:
+virtual void display(){//Virtual Function
+cout<<"\nThis is Parent Class Display Method";
+
+}
+
+};
+
+class Derived: public Parent{
+public:
+void display(){
+cout<<"\nThis is Derived Class Display Method";
+
+}
+
+};
+class Derived1: public Parent{
+public:
+void display(){
+
+cout<<"\nThis is Derived1 class Display Method";
+
+}
+
+};
+
+int main(){
+Parent *p1;//Base class Pointer:
+
+Derived obj1;
+p1=&obj1;//Initilized Base class Pointer to Derived class Reference
+
+p1->display();
+Derived1 obj2;
+p1=&obj2;//Initilized Base class Pointer to Derived1 class Reference
+
+p1->display();
+
+return 0;
+}
