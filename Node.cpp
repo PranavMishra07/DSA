c8f77d3662c4a63e8e4fce24d52d89f0b4944df3
PranavMishra07 <pranav072003@gmail.com>
2024-12-24 13:57:55 +0530

    j

---
+++ Node.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Node{
+	public:
+	int data;
+	Node *next;
+	Node(int data){
+		this->data=data;
+		next=NULL;
+		cout<<"\nNode created";
+	}
+};
+int main(){
+	Node *first=new Node(10);
+	Node *second=new Node(20);
+	Node *third=new Node(30);
+	Node *head=first;
+	head->next=second;
+    head->next->next=third;
+	
+	cout<<"\n"<<head->data<<"==>"<<head->next->data<<"==>"<<head->next->next->data;
+	
+}
+
